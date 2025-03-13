980f709a63323d077562e50aaf2bcf6ffc566e2b
gauthierreinold821 <gauthierreinold821@gmail.com>
2025-03-13 11:24:08 +0000

    Add 8.js

---
+++ src/8.js
@@ -0,0 +1,7 @@
+const http = require('http');
+const port = 3000;
+
+http.createServer((req, res) => {
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  res.end('<h1>Hello World!</h1>');
+}).listen(port);
