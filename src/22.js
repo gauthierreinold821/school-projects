db79b3f67bf1bbf09467631d03d4502c0eb66a08
gauthierreinold821 <gauthierreinold821@gmail.com>
2025-04-01 08:00:29 +0000

    Add 22.js

---
+++ src/22.js
@@ -0,0 +1,16 @@
+function addNode(node) {
+  // Implement the logic to add nodes to your database here
+}
+
+addNode({
+  id: "1",
+  name: "Alice",
+  age: 20,
+  hobbies: ["reading", "coding"]
+});
+
+function removeNode(id) {
+  // Implement the logic to delete a node from your database here
+}
+
+removeNode("1");
